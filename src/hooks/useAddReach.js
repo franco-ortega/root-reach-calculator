49fdb815fc454924743a98a963c8ea155c715dbf
Franco Ortega <francopdx@gmail.com>
2023-09-01 13:52:45 -0700

    remove unused hook

--- src/hooks/useAddReach.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export const useAddReach = (factionReach) => {
-  const [reach, useReach] = useState(0);
-
-  useReach((prev) => prev + factionReach);
-
-  return { reach };
-};
