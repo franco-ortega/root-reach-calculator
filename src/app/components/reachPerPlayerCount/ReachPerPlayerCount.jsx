77901e6e8412184de0a91afc757efe199cd370e0
Franco Ortega <francopdx@gmail.com>
2023-09-08 12:05:22 -0700

    rename ReachBy... to ReachPer and add initial styles

--- src/app/components/reachPerPlayerCount/ReachPerPlayerCount.jsx
+++ src/app/components/reachPerPlayerCount/ReachPerPlayerCount.jsx
@@ -1,17 +1,18 @@
 import reach from '../../../data/reach';
+import styles from './ReachPerPlayerCount.module.css';
 
-export default function ReachByPlayerCount() {
+export default function ReachPerPlayerCount() {
   console.log(reach);
   const data = Object.entries(reach);
   console.log(data);
   return (
-    <div>
+    <div className={styles.ReachPerPlayerCount}>
       <h2>Reach per Player Count</h2>
       <ul>
         {data.map((playerCount) => (
           <li key={playerCount[0]}>
             <span>{playerCount[0]} Players</span> -{' '}
-            <span>Reach - {playerCount[1]}</span>
+            <span>Reach: {playerCount[1]}</span>
           </li>
         ))}
       </ul>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import FactionList from './components/factionList/FactionList';
-import ReachByPlayerCount from './components/reachByPlayerCount/ReachByPlayerCount';
+import ReachPerPlayerCount from './components/reachPerPlayerCount/ReachPerPlayerCount';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Root Reach Calculator</h1>
-      <ReachByPlayerCount />
+      <ReachPerPlayerCount />
       <FactionList />
     </main>
   );
