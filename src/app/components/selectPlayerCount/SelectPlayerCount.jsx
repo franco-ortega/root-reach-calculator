d048034ba70b4f9d2e86800b619ac2175115cf04
Franco Ortega <francopdx@gmail.com>
2023-09-12 14:50:55 -0700

    fine tune initial styles

--- src/app/components/selectPlayerCount/SelectPlayerCount.jsx
+++ src/app/components/selectPlayerCount/SelectPlayerCount.jsx
@@ -1,12 +1,12 @@
-import styles from './SelectPlayerCount.module.css';
 import reachData from '../../../data/reach';
+import styles from './SelectPlayerCount.module.css';
 
 export default function SelectPlayerCount({ setReach }) {
   const reachList = Object.entries(reachData);
 
   return (
     <div className={styles.SelectPlayerCount}>
-      <p>Select Player Count</p>
+      <h3>Select Player Count</h3>
       <select onChange={({ target }) => setReach(Number(target.value))}>
         <option></option>
         {reachList.map((playerCount) => (
