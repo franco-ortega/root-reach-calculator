4e2cccff7e616748254a41c51f942a7ee8faa9e7
Franco Ortega <francopdx@gmail.com>
2023-09-12 15:05:39 -0700

    rename factions to factionsData for clarity and consistency

--- src/app/components/factionList/FactionList.jsx
+++ src/app/components/factionList/FactionList.jsx
@@ -1,13 +1,13 @@
 'use client';
 
-import factions from '../../../data/factions';
+import factionsData from '../../../data/factions';
 import Faction from '../faction/Faction';
 import styles from './FactionList.module.css';
 
 export default function FactionList({ setSelectedFactions }) {
   return (
     <ul className={styles.FactionList}>
-      {factions.map(({ title, reach }) => (
+      {factionsData.map(({ title, reach }) => (
         <Faction
           key={title}
           title={title}
