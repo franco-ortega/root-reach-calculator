eb55a02c621c6aa6b3f4b341b7162095a56d749d
Franco Ortega <francopdx@gmail.com>
2023-09-15 16:42:22 -0700

    destructure playerCount and reach

--- src/components/reachPerPlayerCount/ReachPerPlayerCount.jsx
+++ src/components/reachPerPlayerCount/ReachPerPlayerCount.jsx
@@ -1,17 +1,16 @@
-import reach from '../../data/reach';
+import reachData from '../../data/reach';
 import styles from './ReachPerPlayerCount.module.css';
 
 export default function ReachPerPlayerCount() {
-  const data = Object.entries(reach);
+  const data = Object.entries(reachData);
 
   return (
     <div className={styles.ReachPerPlayerCount}>
       <h2>Reach per Player Count</h2>
       <ul>
-        {data.map((playerCount) => (
-          <li key={playerCount[0]}>
-            <span>{playerCount[0]} Players</span> -{' '}
-            <span>Reach: {playerCount[1]}</span>
+        {data.map(([playerCount, reach]) => (
+          <li key={playerCount}>
+            <span>{playerCount} Players</span> - <span>Reach: {reach}</span>
           </li>
         ))}
       </ul>
