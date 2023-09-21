e28db713a9000427d8bc8995ac267a967b8845a1
Franco Ortega <francopdx@gmail.com>
2023-09-20 17:22:29 -0700

    move main styles from home page to Main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,10 @@
 import ReachPerPlayerCount from '../reachPerPlayerCount/ReachPerPlayerCount';
 import SelectPlayersAndFactions from '../selectPlayersAndFactions/SelectPlayersAndFactions';
+import styles from './Main.module.css';
 
 export default function Main() {
   return (
-    <main>
+    <main className={styles.Main}>
       <ReachPerPlayerCount />
       <SelectPlayersAndFactions />
     </main>
