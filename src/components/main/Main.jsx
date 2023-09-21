69db27241d038ab94d366c67bad5e1f97413a370
Franco Ortega <francopdx@gmail.com>
2023-09-20 17:21:13 -0700

    create Main component for main content of app

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import ReachPerPlayerCount from '../reachPerPlayerCount/ReachPerPlayerCount';
+import SelectPlayersAndFactions from '../selectPlayersAndFactions/SelectPlayersAndFactions';
+
+export default function Main() {
+  return (
+    <main>
+      <ReachPerPlayerCount />
+      <SelectPlayersAndFactions />
+    </main>
+  );
+}
